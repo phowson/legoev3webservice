4b6162921ed48cdf855781eafde798c9f066d78e
phowson <phil@howson.net>
2017-12-28 18:56:41 +0000

    initial

---
+++ legoev3webservice/src/main/java/phil/legoev3webservice/App.java
@@ -0,0 +1,13 @@
+package phil.legoev3webservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
