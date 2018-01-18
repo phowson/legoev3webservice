a7b86751d3721e485a470af64acd124acfb2d048
phowson <phil@howson.net>
2018-01-18 16:53:54 +0000

    cleaning up edge cases

--- legoev3webservice/src/main/java/phil/legoev3webservice/server/RobotControlServer.java
+++ legoev3webservice/src/main/java/phil/legoev3webservice/server/RobotControlServer.java
@@ -3,6 +3,8 @@ package phil.legoev3webservice.server;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.ClosedSelectorException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -37,48 +39,58 @@ public class RobotControlServer implements Closeable {
 		// you can't really get the robot to do more than one thing at once.
 		logger.info("Starting server");
 		while (!stopped) {
-			int s = sel.select();
-			if (s > 0) {
-
-				for (Iterator<SelectionKey> it = sel.selectedKeys().iterator(); it.hasNext();) {
-					SelectionKey k = it.next();
-					final SelectableChannel channel = k.channel();
-					if (k.isAcceptable()) {
-						ServerSocketChannel c2 = (ServerSocketChannel) channel;
-						SocketChannel c = c2.accept();
-						c.configureBlocking(false);
-						c.register(sel, SelectionKey.OP_READ, new Session(this, c, robotController));
-						logger.info("Accepting : " + c);
-					} else if (k.isReadable()) {
-						try {
-							((Session) k.attachment()).onReadable(channel);
-						} catch (Exception ex) {
-							logger.error("Unexpected Exception on channel " + channel, ex);
-							try {
-								channel.close();
-							} catch (Exception ex2) {
-							}
-
-							k.cancel();
-						}
-					} else {
-
-						SocketChannel c = (SocketChannel) channel;
-						logger.info("Closing and deregistering " + c);
-						try {
-							c.close();
-						} catch (Exception ex2) {
-						}
-						k.cancel();
-					}
-
-					it.remove();
+			try {
+				int s = sel.select();
+				if (s > 0) {
+					checkSelector();
 				}
-
+			} catch (ClosedSelectorException ex) {
+				logger.info("Selector closed. Exiting.");
+				break;
 			}
 
 		}
 
+	}
+
+	private void checkSelector() throws IOException, ClosedChannelException {
+		for (Iterator<SelectionKey> it = sel.selectedKeys().iterator(); it.hasNext();) {
+			SelectionKey k = it.next();
+			final SelectableChannel channel = k.channel();
+			if (k.isValid()) {
+
+				if (k.isAcceptable()) {
+					ServerSocketChannel c2 = (ServerSocketChannel) channel;
+					SocketChannel c = c2.accept();
+					c.configureBlocking(false);
+					c.register(sel, SelectionKey.OP_READ, new Session(this, c, robotController));
+					logger.info("Accepting : " + c);
+				} else if (k.isReadable()) {
+					try {
+						((Session) k.attachment()).onReadable(channel);
+					} catch (Exception ex) {
+						logger.error("Unexpected Exception on channel " + channel, ex);
+						try {
+							channel.close();
+						} catch (Exception ex2) {
+						}
+
+						k.cancel();
+					}
+				} else {
+
+					SocketChannel c = (SocketChannel) channel;
+					logger.info("Closing and deregistering " + c);
+					try {
+						c.close();
+					} catch (Exception ex2) {
+					}
+					k.cancel();
+				}
+			}
+
+			it.remove();
+		}
 	}
 
 	public void unregister(SelectableChannel selectableChannel) {
